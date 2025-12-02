75a34266774cfacea160db92a3ccd001e5013d70
Saurav s Mehta <sauravmehta631@gmail.com>
2025-12-02 18:01:02 +0530

    added

--- index.js
+++ index.js
@@ -4,31 +4,23 @@ main().then(() => console.log('Connected to MongoDB'))
 .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  await mongoose.connect('mongodb://127.0.0.1:27017/amazon');
 }
 
-const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    age : Number
+const bookSchema = new mongoose.Schema({
+    title : {
+        type : String,
+        required : true
+    },
+    author :{
+        type : String,
+    },
+    price : {
+        type : Number,
+        required : true
+    }
 });
 
-const User = mongoose.model("User",userSchema);
-
-User.insertMany([
-    {name:"gaurav",email:"gaurav@gmail.com", age : 22},
-    {name: "mayank",email:"mayank@gmail.com",age:21},
-    {name:"harry",email: "harry@gmail.com",age:30}
-])
-.then((res)=>{
-    console.log(res);
-})
-.catch(err=>console.log(err));
+let Book = mongoose.model("Book",bookSchema);
 
 
-User.find().then((data)=>{
-    console.log(data);
-})
-
-User.findById({_id:"692d4c7204469094dfeabfa1"})
-.then(data=>console.log(data));
